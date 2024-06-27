bb48c0a52350bfc3de8d56335d1789d7913a6521
allison-0 <allisonulrich00@gmail.com>
2024-06-27 15:33:21 -0700

    commit

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     number = int(input('Enter your input: '))
     result = []
     while number > 0:
-
+        remainder = number % 2
+        result.append(remainder)
+        number = number // 2
 
     print(*result)
     ########################################
